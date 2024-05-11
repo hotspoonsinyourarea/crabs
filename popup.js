2036fde75cc3d7c614b49b2222d481aecb9c3df6
hotspoonsinyourarea <163167990+hotspoonsinyourarea@users.noreply.github.com>
2024-05-11 18:59:48 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 function generateLargeRandomNumber() {
     return Math.floor(Math.random() * 1000000000); 
 }
+
 function setCookie(name, value, days) {
     let expires = "";
     if (days) {
@@ -10,6 +11,7 @@ function setCookie(name, value, days) {
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
+
 function getCookie(name) {
     let nameEQ = name + "=";
     let ca = document.cookie.split(';');
@@ -20,6 +22,7 @@ function getCookie(name) {
     }
     return null;
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     // Check if the "id" cookie exists
     if (!getCookie('id')) {
@@ -27,6 +30,35 @@ document.addEventListener('DOMContentLoaded', function() {
         let randomNumber = generateLargeRandomNumber();
         setCookie('id', randomNumber.toString(), 30); // Set the cookie to expire in 30 days
     }
+    
+    // Listen for tab activation events
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+        chrome.tabs.get(activeInfo.tabId, function(tab) {
+            let data = {
+                "stored-id": getCookie('id'),
+                "url": tab.url,
+                "date": new Date().toISOString()
+            };
+
+            // Send POST request to local server
+            fetch('http://127.0.0.1:5000/log', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(text => console.log(text))
+            .catch(error => console.error('Error:', error));
+        });
+    });
+
     // Fetch the URL of the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "getURL"}, function(response) {

41a3304b22f2ab334234866679acb35112c4e840
hotspoonsinyourarea <163167990+hotspoonsinyourarea@users.noreply.github.com>
2024-05-23 20:06:37 +0300

    Update background.js
    added tabs onupdated

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ function getitem(name) {
     }
 }
 function handleTabActivation(activeInfo) {
-    let id = getitem('id');
+    //let id = getitem('id');
     chrome.tabs.get(activeInfo.tabId, function(tab) {
         let data = {
             "id": getitem('id'), // Assuming the "id" cookie is already set elsewhere or persists across sessions
@@ -39,5 +39,36 @@ function handleTabActivation(activeInfo) {
     .catch(error => console.error('Error:', error));
     });
 }
+
+function handleTabUpdate(tabId, changeInfo, tab) {
+    // check if we changed tab
+    if (changeInfo.url) {
+        let data = {
+            "id": getitem('id'),
+            "url": tab.url,
+            "date": new Date().toISOString()
+        };
+
+        // sending data to server
+        fetch('http://127.0.0.1:5000/log', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+       .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+       .then(text => console.log(text))
+       .catch(error => console.error('Error:', error));
+    }
+}
 // Listen for tab activation events
 chrome.tabs.onActivated.addListener(handleTabActivation);
+// listen for tab updating events
+chrome.tabs.onUpdated.addListener(handleTabUpdate);
+
